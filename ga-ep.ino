7a13d490e2d204ed95af258b0d7084dfc1c2ece5
erik-gustafsson-nti-johanneberg <42681756+MrErik7@users.noreply.github.com>
2023-12-09 12:58:05 +0100

    v.1.3.1
    - minor changes

--- ga-ep.ino
+++ ga-ep.ino
@@ -3,8 +3,8 @@
 
 // -- Pancake cooking configuration variables --
 bool firstTimeSetup = true;
-int valveTime = 500; // (miliseconds) on how the long the valve should be open 
-int cookTime = 2000; // (miliseconds) on how long both the plates should cook the pancake
+int valveTime = 10000; // (miliseconds) on how the long the valve should be open 
+int cookTime = 1200000; // (miliseconds) on how long both the plates should cook the pancake
 
 // Define the stepper motor pins
 const int stepPinX = 2;
@@ -26,7 +26,6 @@ Motor motorY = Motor(stepPinY, dirPinY, delayY);
 
 // Define the servo motor pins
 const int servo_pin = 12;
-int servo_pos = 0;
 
 // Initialize the servo
 Servo servo;
@@ -38,61 +37,31 @@ void setup() {
 
     // --SERVO SETUP--
     servo.attach(servo_pin);
+    servo.write(180);              // tell servo to go to position in variable 'pos'
 
     // -- DEBUG --
     Serial.begin(9600);
     Serial.println("initialized");
 
-   // Move up the top plate
- // motorY.setDirection(HIGH);
-//  motorY.moveTo(1000);
+
+  // cookPancake();
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   // Open the valve 
-   // cookPancake();
+ // cookPancake();
+//
 
+  
+  motorX.setDirection(HIGH);
+  motorX.moveTo(100);
+  delay(1000);
 
   motorX.setDirection(LOW);
-  motorX.moveTo(200);
-
-  delay(1000);
-
-  motorX.setDirection(HIGH);
   motorX.moveTo(0);
-  delay(1000);
-
-
-  /*
-  for (x = servo_pos; x <= 45; x += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    servo.write(servo_pos);              // tell servo to go to position in variable 'pos'
-    servo_pos+=1;
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-
-  // Wait for a moment before closing the valve
-  delay(valveTime);
-
-  // Close the valve
-  for (x = servo_pos; x >= 0; x -= 1) { // goes from 180 degrees to 0 degrees
-    servo.write(servo_pos);              // tell servo to go to position in variable 'pos'
-    servo_pos-=1;
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  */
-
-
-  /*
-  motorY.setDirection(HIGH);
-  motorY.moveTo(200);
-  delay(1000);
-
-  motorY.setDirection(LOW);
-  motorY.moveTo(0);
-  delay(1000); */
+  delay(1000); 
 
 
 }
@@ -109,22 +78,22 @@ void cookPancake() {
   Serial.println("---PANCAKE COOK HAS STARTED---");
   
   // Open the valve 
-  for (int x = servo_pos; x <= 45; x += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    servo.write(servo_pos);              // tell servo to go to position in variable 'pos'
-    servo_pos+=1;
+  for (int x = 180; x >= 0; x -= 1) { // goes from 180 degrees to 0 degrees
+    servo.write(x);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
+
 
   // Wait for a moment before closing the valve
-  delay(valveTime);
+  delay(10000);
 
   // Close the valve
-  for (int x = servo_pos; x >= 0; x -= 1) { // goes from 180 degrees to 0 degrees
-    servo.write(servo_pos);              // tell servo to go to position in variable 'pos'
-    servo_pos-=1;
+  for (int x = 0; x <= 180; x += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    servo.write(x);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
+
 
   // Move down the top plate
   motorY.setDirection(LOW);
@@ -134,21 +103,21 @@ void cookPancake() {
   // Then wait
   Serial.println("---PANCAKE IS BEING COOKED FROM BOTH SIDES---");
   Serial.println("---PLEASE WAIT---");
-  delay(cookTime);
+  delay(60000);
 
   // Move up the top plate
   motorY.setDirection(HIGH);
   motorY.moveTo(1000);
 
   // And then rotate the lower plate
-  motorX.setDirection(HIGH);
+  motorX.setDirection(LOW);
   motorX.moveTo(100);
 
   // Wait for a second
   delay(1000);
 
   // Then rotate it back
-  motorX.setDirection(LOW);
+  motorX.setDirection(HIGH);
   motorX.moveTo(0);
 
   // THen finished
